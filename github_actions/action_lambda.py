4b98ed4191b4f381c2bfdaf549230e1db91cad55
Devendra <sfacebook030@gmail.com>
2023-04-18 11:30:13 +0530

    adding changes

--- github_actions/action_lambda.py
+++
@@ -1,7 +0,0 @@
-import json
-
-
-def lambda_handler(event, context):
-    message = "Hello there! This is a samaple test lamdba for github actions"
-
-    return {"statusCode": 200, "body": json.dumps(message)}
